15a35d5cf5fbf10e0b4361573b50d55c80f77711
syeon8344 <yeon8344@gmail.com>
2024-08-23 16:11:33 +0900

    0823 PM

--- src/day08/task12/service.py
+++ src/day08/task12/service.py
@@ -12,14 +12,17 @@ while True:  # 데이터 부분
     line = f.readline().strip().replace("\"","").split(",")
     if line == ['']:  # 공백이면 끝
         break
-    data.append(line)  # data 리스트에 line 추가
+    # 데이터 CSV상 순서 (인덱스 찾기용)
+    # "X","시군구","X","X","X","단지명","전용면적(㎡)","계약년월","계약일","거래금액(만원)[금액1,금액2]","동","층","X","X","X","X","X","X","X","X"
+    contract_amount = ''.join([line[9], line[10]])
+    data.append({"location": line[1], "name": line[5], "area": line[6],
+                 "year_month": line[7], "day": line[8],"contract_amount": contract_amount, "bldg_no": line[11], "floor": line[12]})  # data 리스트에 line 추가
+print(data)
 
-
-for row in data:
-
-
-def getall():
-
+# [4] 단지명 별로 거래량 계산해서 거래량이 많은 단지명 TOP10 출력
+def top_ten_transaction():
+    top_list = []  # 출력할 거래량 순위 목록
+    complex_name = []  # 단지명 리스트
 
 
     return
